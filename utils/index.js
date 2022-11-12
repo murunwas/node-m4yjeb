df78c03f41a28165bf7f4c1af0a31d8fab78aefa
murunwas <cengyweb@gmail.com>
2022-11-12 14:31:37 +0200

    all

--- utils/index.js
+++ utils/index.js
@@ -1,34 +1,67 @@
-const dataForge = require('data-forge');
-require('data-forge-fs');
-// require('data-forge-plot');
-// require('@data-forge-plot/render');
-const {log} = console;
+const dataForge = require("data-forge");
+require("data-forge-fs");
+const Enumerable = require("linq");
+const ccxt = require("ccxt");
+const EMA = require("technicalindicators").EMA;
+require('data-forge-plot'); // Extends Data-Forge with the 'plot' function.
+require('@plotex/render-image'); // Extends Data-Forge Plot with the 'renderImage' function.
 
-class Bot{
-    constructor(){
+const { log } = console;
 
-    }
+class Bot {
+  constructor() {}
 
-    get df(){
-        return new dataForge.DataFrame({
-            columnNames:["id","name","sex","age"],
-            rows:[
-                [1,"Jesse","male",25],
-                [2,"Jane","female",25],
-                [3,"Mark","male",20],
-                [4,"Peter","male",55],
-                [5,"Paula","female",35],
-        
-            ]
-        }).setIndex("id")
-        .dropSeries("id");
-    }
+  async getData() {
+    const SYMBOL = "BTC/USDT";
+    const INTERVAL = "15m";
+    let binance = new ccxt.binance();
+    let data = await binance.fetchOHLCV(SYMBOL, INTERVAL);
+    const df = data.map((d) => ({
+      time: d[0] / 1000,
+      open: d[1] * 1,
+      high: d[2] * 1,
+      low: d[3] * 1,
+      close: d[4] * 1,
+      volume: d[5] * 1,
+    }));
+    return new dataForge.DataFrame(df).setIndex("time");
+  }
 
-    dd(){
-        log(this.df.head(2).toString())
-        log(this.df.getSeries('name').toArray())
-        //this.df.plot().renderImage("my-chart.png");
-    }
+  async dd() {
+    let df = await this.getData();
+    df.plot().renderImage("my-chart.png");
+    //df = this.calcEma(df, 20);
+    //console.log(df.tail(12).toString());
+    //log(this.df.getSeries('name').toArray())
+    //this.df.plot().renderImage("my-chart.png");
+    //     var subsetToPlot = this.df.subset(["name", "sex"]);
+    //     var data = Enumerable.from(subsetToPlot.toArray()) // Assume first column is date, second column is value to plot.
+    //       .select(function (entry) {
+    //         return [entry.name, entry.sex];
+    //       })
+    //       .toArray();
+
+    //     log(data);
+  }
+
+  sma(dataFrame, period) {
+    var movingAvg = dataFrame
+      .getSeries("close")
+      .rollingWindow(period)
+      .select((window) => [window.getIndex().last(), window.average()])
+      .withIndex((pair) => pair[0])
+      .select((pair) => pair[1]);
+
+    // Create a new data frame with the new column, doesn't modify original data frame.
+    //console.log(movingAvg.getIndex().toArray());
+    return dataFrame.withSeries("SMA", movingAvg);
+  }
+
+  calcEma(dataFrame,period=20){
+    const values=dataFrame.getSeries("close").toArray()
+    const ema = EMA.calculate({period , values });
+   return dataFrame.withSeries(`ema${period}`, ema);
+  }
 }
 
 module.exports = new Bot();
