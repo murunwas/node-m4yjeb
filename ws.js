a21d93cde7b92016fc72ba55672bc7be354b1b38
murunwas <cengyweb@gmail.com>
2022-11-14 14:40:46 +0200

    alldf

--- ws.js
+++ ws.js
@@ -3,16 +3,26 @@ const { log, error ,clear} = console;
 const Websocket = require('ws');
 const socket = new Websocket('wss://stream.binance.com:9443/ws/bnbusdt@kline_15m');
 
+const candle=(d)=>({
+    time: d.t,
+    open: d.o * 1,
+    high: d.h * 1,
+    low: d.l* 1,
+    close: d.c * 1,
+    volume: d.v * 1,
+  })
+
 let data=[]
 const updatePrice=(d)=>{
+let current=candle(d);
 const f = data[data.length-1]
-log(f)
- log(d.o*1>d.c*1?"bull":"bear",d.o,d.c)
+log("candle height",current.close-current.open)
+ log(current.close*1>current.open*1?"bull":"bear",d.o,d.c)
  //log(d)
 }
 
 const addLine= (d)=>{
-    data.push(d)
+    data.push(candle(d))
 }
 // When message received from web socket then...
 socket.onmessage = function (event) {
@@ -20,9 +30,6 @@ socket.onmessage = function (event) {
     // Easier and shorter.
     var data = JSON.parse(event.data);
     if (data.k.x === true) {
-        log("Add line.");
-
-        // Adding a line with my custom function.
         addLine(data.k);
     } else {
         // Updating line with my custom function.
